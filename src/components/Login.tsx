e2e16f473f086fa62377d18131a54df9287104c9
yassinkazibwe <kazibweusama@gmail.com>
2025-03-01 08:42:24 +0300

    babel

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { useAuth0 } from '@auth0/nextjs-auth0';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Login = () => {
   const {loginWithRedirect} = useAuth0();
